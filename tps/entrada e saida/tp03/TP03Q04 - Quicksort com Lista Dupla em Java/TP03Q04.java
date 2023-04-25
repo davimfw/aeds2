62612049bd5120c365e5895232e76b060eb7a00f
davimfw <davi.freitas@sga.pucminas.br>
2023-04-25 16:02:09 -0300

    feat: TP03Q04 pt1

---
+++ tps/entrada e saida/tp03/TP03Q04 - Quicksort com Lista Dupla em Java/TP03Q04.java
@@ -0,0 +1,498 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+public class TP03Q04 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        MyIO.setCharset("UTF-8");
+        String word = MyIO.readLine();
+        Lista personagens = new Lista();
+        do {
+            personagens.inserirInicio(new Personagem(getFileData(word)));
+            word = MyIO.readLine();
+        } while (!isFim(word));
+
+        personagens.sort();
+        personagens.mostrar();
+    }
+
+    public static String getFileData(String word) {
+        BufferedReader br = null;
+        FileReader fr = null;
+        String fileData = "";
+        try {
+            fr = new FileReader(word.substring(1));
+            br = new BufferedReader(fr);
+
+            // Ler cada linha do arquivo
+            fileData = br.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Fechar o BufferedReader e FileReader
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return fileData;
+    }
+
+    public static boolean isFim(String word) {
+        return word.length() == 3 && word.charAt(0) == 'F' && word.charAt(1) == 'I' && word.charAt(2) == 'M';
+    }
+
+}
+
+class Celula {
+    public Personagem elemento;
+    public Celula ant;
+    public Celula prox;
+
+    /**
+     * Construtor da classe.
+     */
+    public Celula() {
+        this(null);
+    }
+
+    /**
+     * Construtor da classe.
+     * 
+     * @param elemento int inserido na celula.
+     */
+    public Celula(Personagem elemento) {
+        this.elemento = elemento;
+        this.ant = this.prox = null;
+    }
+}
+
+class Lista {
+    private Celula primeiro;
+    private Celula ultimo;
+
+    /**
+     * Construtor da classe que cria uma lista dupla sem elementos (somente no
+     * cabeca).
+     */
+    public Lista() {
+        primeiro = new Celula();
+        ultimo = primeiro;
+    }
+
+    public void sort() {
+        quicksort(primeiro.prox, ultimo);
+    }
+
+    public boolean comparePersonagem(Personagem p1,Personagem p2) {
+        if(!p1.getCorDoCabelo().equals("none") && !p2.getCorDoCabelo().equals("none")) {
+            return p1.getCorDoCabelo().compareToIgnoreCase(p2.getCorDoCabelo()) < 0;
+        } else if(p1.getCorDoCabelo().equals("none") && p2.getCorDoCabelo().equals("none")) {
+            return p1.getNome().compareToIgnoreCase(p2.getNome()) < 0;
+        } else if(p1.getCorDoCabelo().equals("none") && !p2.getCorDoCabelo().equals("none")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void quicksort(Celula esq, Celula dir) {
+        Celula i = esq, j = dir;
+        String pivo = esq.elemento.getCorDoCabelo();
+        while (i != j && i != null && j != null) {
+            while (i != j && i.prox != null && i.elemento.getCorDoCabelo().compareTo(pivo) < 0)
+                i = i.prox;
+            while (j.ant != null && j != i && j.elemento.getCorDoCabelo().compareTo(pivo) > 0)
+                j = j.ant;
+            if (i != j && i.elemento != null && j.elemento != null) {
+                swap(i, j);
+                i = i.prox;
+                j = j.ant;
+            }
+        }
+        if (esq != j)
+            quicksort(esq, j);
+        if (i != dir)
+            quicksort(i, dir);
+    }
+
+    public void swap(Celula i, Celula j) {
+        Personagem tmp = i.elemento.clone();
+        i.elemento = j.elemento.clone();
+        j.elemento = tmp;
+    }
+
+    /**
+     * Insere um elemento na primeira posicao da lista.
+     * 
+     * @param x int elemento a ser inserido.
+     */
+    public void inserirInicio(Personagem x) {
+        Celula tmp = new Celula(x);
+
+        tmp.ant = primeiro;
+        tmp.prox = primeiro.prox;
+        primeiro.prox = tmp;
+        if (primeiro == ultimo) {
+            ultimo = tmp;
+        } else {
+            tmp.prox.ant = tmp;
+        }
+        tmp = null;
+    }
+
+    /**
+     * Insere um elemento na ultima posicao da lista.
+     * 
+     * @param x int elemento a ser inserido.
+     */
+    public void inserirFim(Personagem x) {
+        ultimo.prox = new Celula(x);
+        ultimo.prox.ant = ultimo;
+        ultimo = ultimo.prox;
+    }
+
+    /**
+     * Remove um elemento da primeira posicao da lista.
+     * 
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista nao contiver elementos.
+     */
+    public Personagem removerInicio() throws Exception {
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+        }
+
+        Celula tmp = primeiro;
+        primeiro = primeiro.prox;
+        Personagem resp = primeiro.elemento;
+        tmp.prox = primeiro.ant = null;
+        tmp = null;
+        return resp;
+    }
+
+    /**
+     * Remove um elemento da ultima posicao da lista.
+     * 
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se a lista nao contiver elementos.
+     */
+    public Personagem removerFim() throws Exception {
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+        }
+        Personagem resp = ultimo.elemento;
+        ultimo = ultimo.ant;
+        ultimo.prox.ant = null;
+        ultimo.prox = null;
+        return resp;
+    }
+
+    /**
+     * Insere um elemento em uma posicao especifica considerando que o
+     * primeiro elemento valido esta na posicao 0.
+     * 
+     * @param x   int elemento a ser inserido.
+     * @param pos int posicao da insercao.
+     * @throws Exception Se <code>posicao</code> invalida.
+     */
+    public void inserir(Personagem x, int pos) throws Exception {
+
+        int tamanho = tamanho();
+
+        if (pos < 0 || pos > tamanho) {
+            throw new Exception("Erro ao inserir posicao (" + pos + " / tamanho = " + tamanho + ") invalida!");
+        } else if (pos == 0) {
+            inserirInicio(x);
+        } else if (pos == tamanho) {
+            inserirFim(x);
+        } else {
+            // Caminhar ate a posicao anterior a insercao
+            Celula i = primeiro;
+            for (int j = 0; j < pos; j++, i = i.prox)
+                ;
+
+            Celula tmp = new Celula(x);
+            tmp.ant = i;
+            tmp.prox = i.prox;
+            tmp.ant.prox = tmp.prox.ant = tmp;
+            tmp = i = null;
+        }
+    }
+
+    /**
+     * Remove um elemento de uma posicao especifica da lista
+     * considerando que o primeiro elemento valido esta na posicao 0.
+     * 
+     * @param posicao Meio da remocao.
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se <code>posicao</code> invalida.
+     */
+    public Personagem remover(int pos) throws Exception {
+        Personagem resp;
+        int tamanho = tamanho();
+
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+
+        } else if (pos < 0 || pos >= tamanho) {
+            throw new Exception("Erro ao remover (posicao " + pos + " / " + tamanho + " invalida!");
+        } else if (pos == 0) {
+            resp = removerInicio();
+        } else if (pos == tamanho - 1) {
+            resp = removerFim();
+        } else {
+            // Caminhar ate a posicao anterior a insercao
+            Celula i = primeiro.prox;
+            for (int j = 0; j < pos; j++, i = i.prox)
+                ;
+
+            i.ant.prox = i.prox;
+            i.prox.ant = i.ant;
+            resp = i.elemento;
+            i.prox = i.ant = null;
+            i = null;
+        }
+
+        return resp;
+    }
+
+    /**
+     * Mostra os elementos da lista separados por espacos.
+     */
+    public void mostrar() {
+        for (Celula i = primeiro.prox; i != null; i = i.prox) {
+            i.elemento.imprimir();
+        }
+    }
+
+    /**
+     * Mostra os elementos da lista de forma invertida
+     * e separados por espacos.
+     */
+    public void mostrarInverso() {
+        System.out.print("[ ");
+        for (Celula i = ultimo; i != primeiro; i = i.ant) {
+            System.out.print(i.elemento + " ");
+        }
+        System.out.println("] "); // Termina de mostrar.
+    }
+
+    /**
+     * Procura um elemento e retorna se ele existe.
+     * 
+     * @param x Elemento a pesquisar.
+     * @return <code>true</code> se o elemento existir,
+     *         <code>false</code> em caso contrario.
+     */
+    public boolean pesquisar(Personagem x) {
+        boolean resp = false;
+        for (Celula i = primeiro.prox; i != null; i = i.prox) {
+            if (i.elemento == x) {
+                resp = true;
+                i = ultimo;
+            }
+        }
+        return resp;
+    }
+
+    /**
+     * Calcula e retorna o tamanho, em numero de elementos, da lista.
+     * 
+     * @return resp int tamanho
+     */
+    public int tamanho() {
+        int tamanho = 0;
+        for (Celula i = primeiro; i != ultimo; i = i.prox, tamanho++)
+            ;
+        return tamanho;
+    }
+}
+
+class Personagem {
+    private String nome;
+    private int altura;
+    private Double peso;
+    private String corDoCabelo;
+    private String codDaPele;
+    private String corDosOlhos;
+    private String anoNascimento;
+    private String genero;
+    private String homeWorld;
+
+    public Personagem() {
+        this.nome = "";
+        this.altura = 0;
+        this.peso = 0.0;
+        this.corDoCabelo = "";
+        this.codDaPele = "";
+        this.corDosOlhos = "";
+        this.anoNascimento = "";
+        this.genero = "";
+        this.homeWorld = "";
+    }
+
+    public Personagem(String fileData) {
+        ler(fileData);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setCorDoCabelo(String corDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+    }
+
+    public String getCorDoCabelo() {
+        return corDoCabelo;
+    }
+
+    public void setCodDaPele(String codDaPele) {
+        this.codDaPele = codDaPele;
+    }
+
+    public String getCodDaPele() {
+        return codDaPele;
+    }
+
+    public void setCorDosOlhos(String corDosOlhos) {
+        this.corDosOlhos = corDosOlhos;
+    }
+
+    public String getCorDosOlhos() {
+        return corDosOlhos;
+    }
+
+    public void setAnoNascimento(String anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setHomeWorld(String homeWorld) {
+        this.homeWorld = homeWorld;
+    }
+
+    public String getHomeWorld() {
+        return homeWorld;
+    }
+
+    public Personagem clone() {
+        Personagem temp = new Personagem();
+        temp.nome = this.nome;
+        temp.altura = this.altura;
+        temp.peso = this.peso;
+        temp.corDoCabelo = this.corDoCabelo;
+        temp.codDaPele = this.codDaPele;
+        temp.corDosOlhos = this.corDosOlhos;
+        temp.anoNascimento = this.anoNascimento;
+        temp.genero = this.genero;
+        temp.homeWorld = this.homeWorld;
+        return temp;
+    }
+
+    public void imprimir() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.################");
+        MyIO.println("## " + this.nome + " ## " + this.altura + " ## " + decimalFormat.format(this.peso) + " ## "
+                + this.corDoCabelo + " ## "
+                + this.codDaPele + " ## " + this.corDosOlhos + " ## " + this.anoNascimento + " ## " + this.genero
+                + " ## " + this.homeWorld + " ## ");
+    }
+
+    public void ler(String fileData) {
+        String temp, value;
+        // readNome
+        fileData = fileData.replace("{", "");
+        temp = fileData.replaceFirst("'name': '", "");
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("height': '", "");
+        setNome(value);
+        // readAltura
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("mass': '", "");
+        if (value.equals("unknown"))
+            setAltura(0);
+        else
+            setAltura(Integer.parseInt(value));
+        // readPeso
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("hair_color': '", "");
+        if (value.equals("unknown"))
+            setPeso(0.0);
+        else
+            setPeso(Double.parseDouble(value.replaceAll(",", ".")));
+        // readCorDoCabelo
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("skin_color': '", "");
+        setCorDoCabelo(value);
+        // readCodDaPele
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("eye_color': '", "");
+        setCodDaPele(value);
+        // readCorDosOlhos
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("birth_year': '", "");
+        setCorDosOlhos(value);
+        // readAnoNascimento
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("gender': '", "");
+        setAnoNascimento(value);
+        // readGender
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        temp = temp.replaceFirst("homeworld': '", "");
+        setGenero(value);
+        // readHomeWorld
+        value = temp.substring(0, temp.indexOf("'"));
+        temp = temp.replaceFirst(value + "', '", "");
+        setHomeWorld(value);
+    }
+}
